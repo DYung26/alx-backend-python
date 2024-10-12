dbb0c57901ce92ac70bfd652f56cc11ba43f672c
DYung26 <theadstore1407@gmail.com>
2024-10-11 18:31:32 -0700

    `Define variables` - Add variables with type annotations to demonstrate basic data types

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+This module defines several variables with type annotations
+to demonstrate basic variable declarations and their types.
+'''
+
+a: int = 1              # An integer variable initialized to 1
+pi: float = 3.14        # A floating-point variable representing the value of π
+i_understand_annotations: bool = True  # A boolean variable indicating
+# understanding of annotations
+school: str = 'Holberton'   # A string variable holding the name of the school
