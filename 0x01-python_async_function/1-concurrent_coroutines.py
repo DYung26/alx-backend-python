5e3f8b876668aeaaf8f4a739f8c259ebcaa4935a
DYung26 <theadstore1407@gmail.com>
2024-10-14 21:07:59 -0700

    `Let's execute multiple coroutines at the same time with async` - Implement wait_n function to asynchronously wait for n random delays and return a sorted list of the delays.

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module provides an asynchronous function to wait for random delays
+and collect the results in a sorted list.
+"""
+import asyncio
+import wait_random from 0-basic_async_syntax
+from typing import List
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Asynchronously waits for a specified number of random delays.
+
+    Args:
+        n (int): The number of random delays to wait for.
+        max_delay (int): The maximum delay in seconds for the random delays.
+
+    Returns:
+        List[float]: A list of random delays (sorted) that were waited for.
+    """
+    random_delays = await async.io.gather(
+            *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return (sorted(random_delays))
